7877706f4c162bc84f6dd6facf1ce2b308c42127
Dani Gordon <lilyluvr723@gmail.com>
2022-02-23 18:16:54 +0100

    it works!

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,9 @@
 //<<<<<Create the variables needed and attach to HTML classes >>>//
 
-//*Variables needed*//: 
-
+//*Variables needed*//:
 
 // * width
-const width = 14
+const width = 14;
 // const gridWidth = 1500;
 // const brickWidth = 105;
 // const paddleWidth = 420;
@@ -28,227 +27,269 @@ let score = 0;
 // const xPosition = currentIndex % width;
 // * y =
 // const yPosition = Math.floor(currentIndex / width);
-//*ball position 
+//*ball position
 // let ballPosition = 133
 
-
-
 const grid = document.querySelector('.grid');
-const ball = document.querySelector('.ball');
+// const ball = document.querySelector('.ball');
 const cells = [];
 const paddle = document.querySelectorAll('.paddle');
-let paddlePosition = 145
+let paddlePosition = 145;
 
-let discoPosition = 133
+let discoPosition = 133;
+let ballMovement;
 
-
-
-//create the grid w/divs & grid classes 
+//create the grid w/divs & grid classes
 function createGrid() {
   for (let i = 0; i < gridCellCount; i++) {
     const gameGrid = document.createElement('div');
     if (i <= 27) {
-      gameGrid.className = 'purple';   //grid classes = .purple, .green, .yellow, .hidden, .paddle
-    } else if  (i >= 28 && i < 56) {
+      gameGrid.className = 'purple'; //grid classes = .purple, .green, .yellow, .hidden, .paddle
+    } else if (i >= 28 && i < 56) {
       gameGrid.className = 'green';
     } else if (i >= 56 && i < 70) {
       gameGrid.className = 'yellow';
-    } else if (i >= 145 && i < 149) { 
+    } else if (i >= 145 && i < 149) {
       gameGrid.className = 'paddle';
-    } else {
-      gameGrid.className = 'hidden';
     }
+    // else {
+    //   gameGrid.className = 'hidden';
+    // }
     gameGrid.setAttribute('data-id', i);
     grid.appendChild(gameGrid);
     cells.push(gameGrid);
-    gameGrid.textContent = i 
+    gameGrid.textContent = i;
   }
 }
 //!133 is taken up by the ball image and moved....need to fix asap
-function addBall() {
+function addDisco() {
   cells[discoPosition].classList.add('ball');
 }
 
-createGrid()
-addBall()
+createGrid();
+addDisco();
 
 
 function addPaddle(position) {
-  cells[position].classList.remove('hidden');
   cells[position].classList.add('paddle');
-  console.log('add function works');
 }
 function removePaddle(position) {
-  cells[position].classList.add('hidden');
   cells[position].classList.remove('paddle');
-  console.log('remove function works');
 }
-
 
 function handleKeys(event) {
-
-  switch (event.keyCode) {    // 4. assign key directions
+  switch (
+    event.keyCode // 4. assign key directions
+  ) {
     case 39: //right arrow    // 5. move the bar with key func
       if (paddlePosition <= 150 - 1) {
-        removePaddle(paddlePosition)
-        paddlePosition++
-        addPaddle(paddlePosition + 3)
-        console.log(paddlePosition)  // 6. make sure it works
+        removePaddle(paddlePosition);
+        paddlePosition++;
+        addPaddle(paddlePosition + 3);
       }
-      break
+      break;
     case 37: //left arrow
       if (paddlePosition >= 140 + 1) {
-        removePaddle(paddlePosition + 3) 
-        paddlePosition --
-        addPaddle(paddlePosition)
-        console.log(paddlePosition)
+        removePaddle(paddlePosition + 3);
+        paddlePosition--;
+        addPaddle(paddlePosition);
       }
-      break
-    default: 
-      console.log('invalid key do nothing')
+      break;
+    case 81:
+      stopGame();
+      break;
+    default:
+      console.log('invalid key do nothing');
   }
-  
 }
-// console.log(cells)
 
-
-document.addEventListener('keyup', handleKeys)
-
+document.addEventListener('keyup', handleKeys);
 
 //!<<<<<<<<<<<functions to move the ball>>>>>>>>>>>>>//
-let onLeftPaddle = [145, 146]
-let onRightPaddle = [147, 148]
-let onPaddle = true
-let onYellowBrick = true
-let onGreenBrick = true
-let onPurpleBrick = true
-let direction = 'rightup' 
-
-
-
-
-function addDisco() {
-  cells[discoPosition].classList.remove('hidden');
-  cells[discoPosition].classList.add('ball');
-  console.log('added disco ball');
-}
+let onLeftPaddle = [145, 146];
+let onRightPaddle = [147, 148];
+let onPaddle = true;
+// let onYellowBrick = true;
+// let onGreenBrick = true;
+// let onPurpleBrick = true;
+let direction = 'leftup';
 
 function removeDisco() {
-  cells[discoPosition].classList.add('hidden');
+  // cells[discoPosition].classList.add('hidden');
   cells[discoPosition].classList.remove('ball');
-  console.log('removed disco ball');
 }
 
 //!make brick disappear when ball moves into location of brick
 
-// When ball hits Yellow Brick --> remove yellow class, add hidden, return ball to paddle 
+// When ball hits Yellow Brick --> remove yellow class, add hidden, return ball to paddle
 function yellowCollision() {
+  if (cells[discoPosition].classList.contains('yellow')) {
+    return true;
+  }
+}
+
+function handleYellowCollision() {
   cells[discoPosition].classList.remove('yellow');
-  cells[discoPosition].classList.add('hidden');
-  console.log('it works')
-  // cells[discoPosition].includes.classList('yellow');
 }
 
 // When ball hits Green Brick --> remove green class, add hidden, return ball to paddle
 function greenCollision() {
-  cells[discoPosition].classList.remove('green'); 
-  cells[discoPosition].classList.add('hidden');
+  if (cells[discoPosition].classList.contains('green')) {
+    return true;
+  }
+}
+
+function handleGreenCollision() {
+  cells[discoPosition].classList.remove('green');
 }
 // When ball hits Purple Brick --> remove purple class, add hidden, return ball to paddle
 function purpleCollision() {
+  if (cells[discoPosition].classList.contains('purple')) {
+    return true;
+  }
+}
+
+function handlePurpleCollision() {
   cells[discoPosition].classList.remove('purple');
-  cells[discoPosition].classList.add('hidden');
 }
 
-function hitBrickMoveDown() {
-  cells[discoPosition].includesClassList('green')
-  discoPosition += 13
+function paddleCollision() {
+  if (cells[discoPosition].classList.contains('paddle')) {
+    console.log('contains paddle')
+    return true;
+  }
 }
 
-function walls() {
-  const leftEdge = discoPosition[0] % width === 0
-  const rightEdge = discoPosition[discoPosition.length - 1] % width === width - 1
-  const topEdge = discoPosition >= 13
+const isCollisionWithWall = () => {
+  const leftWall = discoPosition % width === 0;
+  const rightWall = discoPosition % width === width - 1;
+  const topWall = discoPosition <= 13;
+  return [leftWall, rightWall, topWall].some((val) => val);
+};
 
+
+
+
+function handleCollisionWithWall() {
+  const leftWall = discoPosition % width === 0;
+  const rightWall = discoPosition % width === width - 1;
+  const topWall = discoPosition <= 13;
+  const paddle = cells[discoPosition].classList.contains('paddle')
+  const goingUp = direction.slice(direction.length - 2) === 'up';
+  console.log({ direction, leftWall, rightWall, topWall, goingUp, paddle });
+  if (leftWall) {
+    if (goingUp) {
+      direction = 'rightup';
+    } else {
+      direction = 'rightdown';
+    }
+  } else if (rightWall) {
+    if (goingUp) {
+      direction = 'leftup';
+    } else {
+      direction = 'leftdown';
+    }
+  } else if (topWall) {
+    const isGoingLeft = direction.slice(4) === 'left';
+    if (isGoingLeft) {
+      direction = 'leftdown';
+    } else {
+      direction = 'rightdown';
+    }
+  } else if (paddle) {
+    const goingDown = direction.slice(direction.length - 4) === 'down';
+    if (goingDown) {
+      direction = 'rightup';
+    } else { 
+      direction = 'leftup';
+    }
+  }
+  console.log('hit wall', direction, paddle,)
 }
 
-// function discoDirection() {
-//   if (direction === 'leftup') {
-//     discoPosition -= 13
-//   } else if (direction === 'leftdown') {
-//     discoPosition += 13
-//   } else if (direction === 'rightup') {
-//     discoPosition -= 15
-//   } else if (direction === 'rightdown') {
-//     discoPosition += 15
-//   }
-// }
-// console.log(direction)
+function changeDiscoDirection() {
+  if (direction === 'rightup') {
+    direction = 'rightdown';
+  }
+  if (direction === 'leftup') {
+    direction = 'leftdown';
+  }
+}
 
+function changeDiscoDirectionTwo() {
+  if (direction === 'rightdown') {
+    direction = 'leftup';
+  }
+  if (direction === 'leftdown') {
+    direction = 'rightup';
+  }
+}
+
+const updateDiscoPosition = () => {
+  if (direction === 'leftup') {
+    discoPosition = discoPosition - 15;
+  } else if (direction === 'leftdown') {
+    discoPosition = discoPosition + 13;
+  } else if (direction === 'rightup') {
+    discoPosition = discoPosition - 13;
+  } else if (direction === 'rightdown') {
+    discoPosition = discoPosition + 15;
+  }
+};
 
 function moveDiscoBall() {
-  const ballMovement = setInterval(() => {
-    if (onPaddle) {
-      removeDisco()
-      discoPosition -= 13
-      yellowCollision()
-      if (yellowCollision) {
-        removeDisco()
-        direction = 'leftdown'
-      }
-      // greenCollision()
-      // purpleCollision()
-      // hitBrickMoveDown()
-      if (onYellowBrick) {
-        if (direction === 'leftup') {
-          direction = 'leftdown';
-        } else if (direction === 'rightup') {
-          direction = 'rightdown';
-        }
-        if (onLeftPaddle) {
-          direction === 'leftup';
-        } else {
-          direction === 'rightup';
-        }
-      }
-      // greenCollision()
-      // yellowCollision()
-      // hitBrickMoveDown(direction)
-      // discoPosition += 13
-      removeDisco()
-      // if (direction === 'leftup') {
-      //   discoPosition -= 13
-      // } else if (direction === 'leftdown') {
-      //   discoPosition += 13
-      // } else if (direction === 'rightup') {
-      //   discoPosition -= 15
-      // } else if (direction === 'rightdown') {
-      //   discoPosition += 15
-      // }
-      addDisco()
-    } else { 
-      clearInterval(ballMovement)
-    } 
-  }, 1000)
+  ballMovement = setInterval(() => {
+    if (yellowCollision()) {
+      changeDiscoDirection();
+      handleYellowCollision();
+    }
+
+    if (greenCollision()) {
+      changeDiscoDirection();
+      handleGreenCollision();
+    }
+
+    if (purpleCollision()) {
+      changeDiscoDirection();
+      handlePurpleCollision();
+    }
+
+    if (paddleCollision()) {
+      changeDiscoDirectionTwo();
+    }
+
+    removeDisco();
+
+    updateDiscoPosition();
+
+    if (isCollisionWithWall()) {
+      handleCollisionWithWall();
+    }
+
+    addDisco();
+
+    if (discoPosition > 153) {
+      clearInterval(ballMovement);
+    }
+  }, 1000);
+}
+
+function stopGame() {
+  console.log('stopping game');
+  clearInterval(ballMovement);
 }
 
 //<<<<<<<<Event Listner for mouseClick on StartGame button>>>>>//
-// start.addEventListener('click', moveDiscoBall)
+start.addEventListener('click', moveDiscoBall);
 //event listner for spacebar
-document.addEventListener('keydown', moveDiscoBall) 
-
-
 
 // make brick disappear when ball moves into location of brick
 
 // make ball move to bottom after ball moves into location of brick
 
-
 //if bar misses ball, turn over, ball returns to start position
 
-
 //assign pts to cell position of bricks
-
 
 // 10. score pts when ball hits brick cell position
 //     - pts determined by location of brick
